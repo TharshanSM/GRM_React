19a9413449ddba5a0bb8111e8705c216f270144a
TharshanSM <ugatharshan95@gmail.com>
2023-07-12 13:46:28 +0530

    MongoDB Cloud Installed

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const url = "mongodb://127.0.0.1:27017/grm";
+//const url = "mongodb://127.0.0.1:27017/grm";
+const url =
+    "mongodb+srv://TharshanSM:TharSM5764@grmtest.x2auozj.mongodb.net/?retryWrites=true&w=majority";
 
 const connect = async () => {
     try {
